85bc73a4060f5267d8f4fd117fe22e722d527ea5
Kai Blin <kblin@biosustain.dtu.dk>
2022-10-25 11:00:29 +0200

    meme: Fix validation errors
    Signed-off-by: Kai Blin <kblin@biosustain.dtu.dk>

--- Formula/meme.rb
+++ Formula/meme.rb
@@ -5,12 +5,7 @@ class Meme < Formula
   url "http://meme-suite.org/meme-software/4.11.2/meme_4.11.2_2.tar.gz"
   version "4.11.2.2"
   sha256 "377238c2a9dda64e01ffae8ecdbc1492c100df9b0f84132d50c1cf2f68921b22"
-  revision 4
-
-  bottle do
-    root_url "https://linuxbrew.bintray.com/bottles-bio"
-    sha256 "e749f456fde1861ebcaac2614f031dc29722e34e2e0c5bdfbe0997a21710c7e4" => :x86_64_linux
-  end
+  revision 5
 
   # Work around the error:
   # ld: file not found: /usr/lib/system/libsystem_darwin.dylib
@@ -38,7 +33,7 @@ class Meme < Formula
     else
       ENV["PERL5LIB"] = libexec/"lib/perl5"
       system "cpanm", "--self-contained", "-l", libexec, "XML::Parser::Expat"
-      (bin/"meme").write_env_script(libexec/"bin/meme", :PERL5LIB => ENV["PERL5LIB"])
+      (bin/"meme").write_env_script(libexec/"bin/meme", PERL5LIB: ENV["PERL5LIB"])
     end
   end
 
